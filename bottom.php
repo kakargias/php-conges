ff7ee68653ed45f72e5a7e365d3f4ca48dd6291b
charlene.sagatova <charlene.sagatova@gmail.com>
2011-11-21 15:24:10 +0000

    add php-conges v1.6 to trunk

---
+++ bottom.php
@@ -0,0 +1,37 @@
+<?php
+
+	defined( '_PHP_CONGES' ) or die( 'Restricted access' );
+	
+	/*************************************/
+	/***  fin de la page             ***/
+	
+	echo "</CENTER>\n";
+
+	
+	echo "<hr align=\"center\" size=\"2\" width=\"90%\"\>\n";
+
+	echo "</div>";
+	echo "</div>";
+	echo "</div>";
+	echo "</div>";
+	echo "</div>";		
+				
+	echo "<div id=\"content\">";
+		
+	echo "<div class=\"ui-corner-all-8\" style=\"background-color: #C11A22; padding: 2px; margin: 10px;\">";
+	echo "<div class=\"ui-corner-all\" style=\"background-color: white; padding: 2px;\">";
+	echo "<div class=\"ui-corner-all\" style=\"background-color: #C3C3C3; padding: 7px; margin: 3px;\">";
+	echo "<CENTER>\n";
+	echo "Adexgroup © 2011 - 31 boulevard Bourdon - 75004 Paris";
+	echo "</CENTER>\n";
+	
+	echo "</div>";
+	echo "</div>";
+	echo "</div>";
+	echo "</div>";
+	
+	
+	echo "</body>\n";
+	echo "</html>\n";
+	
+?>

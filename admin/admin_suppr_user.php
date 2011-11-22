e61ac573b9d3ea40cf9d10776268f1cb444501e6
holblin <holblin@gmail.com>
2011-11-22 08:36:57 +0000

    add old tags from http://www.ced.univ-montp2.fr/php_conges/

--- admin/admin_suppr_user.php
+++ admin/admin_suppr_user.php
@@ -1,212 +1,155 @@
 <?php
 /*************************************************************************************************
-PHP_CONGES : Gestion Interactive des CongÃ©s
+PHP_CONGES : Gestion Interactive des Congés
 Copyright (C) 2005 (cedric chauvineau)
 
-Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique GÃ©nÃ©rale GNU publiÃ©e par la Free Software Foundation.
-Ce programme est distribuÃ© car potentiellement utile, mais SANS AUCUNE GARANTIE,
-ni explicite ni implicite, y compris les garanties de commercialisation ou d'adaptation
-dans un but spÃ©cifique. Reportez-vous Ã  la Licence Publique GÃ©nÃ©rale GNU pour plus de dÃ©tails.
-Vous devez avoir reÃ§u une copie de la Licence Publique GÃ©nÃ©rale GNU en mÃªme temps
-que ce programme ; si ce n'est pas le cas, Ã©crivez Ã  la Free Software Foundation,
-Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, Ã‰tats-Unis.
+Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les 
+termes de la Licence Publique Générale GNU publiée par la Free Software Foundation.
+Ce programme est distribué car potentiellement utile, mais SANS AUCUNE GARANTIE, 
+ni explicite ni implicite, y compris les garanties de commercialisation ou d'adaptation 
+dans un but spécifique. Reportez-vous à la Licence Publique Générale GNU pour plus de détails.
+Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même temps 
+que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation, 
+Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, États-Unis.
 *************************************************************************************************
 This program is free software; you can redistribute it and/or modify it under the terms
-of the GNU General Public License as published by the Free Software Foundation; either
+of the GNU General Public License as published by the Free Software Foundation; either 
 version 2 of the License, or any later version.
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
 See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 *************************************************************************************************/
 
-define('_PHP_CONGES', 1);
-defined( '_PHP_CONGES' ) or die( 'Restricted access' );
-
-$session=(isset($_GET['session']) ? $_GET['session'] : ((isset($_POST['session'])) ? $_POST['session'] : session_id()) ) ;
-
+include("../config.php") ;
 include("../fonctions_conges.php") ;
 include("../INCLUDE.PHP/fonction.php");
 include("../INCLUDE.PHP/session.php");
+if($config_verif_droits==1){ include("../INCLUDE.PHP/verif_droits.php");}
+?>
 
-
-$DEBUG=FALSE;
-//$DEBUG=TRUE ;
-
-// verif des droits du user Ã  afficher la page
-verif_droits_user($session, "is_admin", $DEBUG);
-
-
-
-echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\">\n";
-echo "<html>\n";
-echo "<head>\n";
-echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n";
-echo "<link href=\"../".$_SESSION['config']['stylesheet_file']."\" rel=\"stylesheet\" type=\"text/css\">\n";
-echo "<link href=\"../style.css\" rel=\"stylesheet\" type=\"text/css\" />";
-echo "<TITLE> ".$_SESSION['config']['titre_admin_index']." </TITLE>\n";
-echo "</head>\n";
-
-	$info="user_supp";
-	include("../menu.php");
-
-
-	$bgimage=$_SESSION['config']['URL_ACCUEIL_CONGES']."/".$_SESSION['config']['bgimage'];
-	echo "<body text=\"#000000\" bgcolor=".$_SESSION['config']['bgcolor']." link=\"#000080\" vlink=\"#800080\" alink=\"#FF0000\" background=\"$bgimage\">\n";
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN">
+<html>
+<head>
+<?php 
+	echo "<link href=\"../$config_stylesheet_file\" rel=\"stylesheet\" type=\"text/css\">\n";
+	echo "<TITLE> CONGES : Administrateur</TITLE>\n";
+	echo "</head>\n";
+	
+	echo "<body text=#000000 bgcolor=$config_bgcolor link=#000080 vlink=#800080 alink=#FF0000 background=\"$URL_ACCUEIL_CONGES/$config_bgimage\">\n";
 	echo "<CENTER>\n";
-
+	
 	/*************************************/
-	// recup des parametres reÃ§us :
+	// recup des parametres reçus :
 	// SERVER
 	$PHP_SELF=$_SERVER['PHP_SELF'];
-	// GET / POST
-	$u_login = getpost_variable("u_login") ;
-	$u_login_to_delete = getpost_variable("u_login_to_delete") ;
+	// GET
+	if(isset($_GET['u_login'])) { $u_login=$_GET['u_login']; }
+	if(isset($_GET['u_login_to_delete'])) { $u_login_to_delete=$_GET['u_login_to_delete']; }
+	// POST
+	if(!isset($u_login_to_delete))
+		if(isset($_POST['u_login_to_delete'])) { $u_login_to_delete=$_POST['u_login_to_delete']; }
+	if(isset($_POST['new_login'])) { $new_login=$_POST['new_login']; }
+	if(isset($_POST['new_nom'])) { $new_nom=$_POST['new_nom']; }
+	if(isset($_POST['new_prenom'])) { $new_prenom=$_POST['new_prenom']; }
+	if(isset($_POST['new_quotite'])) { $new_quotite=$_POST['new_quotite']; }
+	if(isset($_POST['new_nb_j_an'])) { $new_nb_j_an=$_POST['new_nb_j_an']; }
+	if(isset($_POST['new_solde_jours'])) { $new_solde_jours=$_POST['new_solde_jours']; }
+	if(isset($_POST['new_is_resp'])) { $new_is_resp=$_POST['new_is_resp']; }
+	if(isset($_POST['new_resp_login'])) { $new_resp_login=$_POST['new_resp_login']; }
+	if(isset($_POST['tab_checkbox_sem_imp'])) { $tab_checkbox_sem_imp=$_POST['tab_checkbox_sem_imp']; }
+	if(isset($_POST['tab_checkbox_sem_p'])) { $tab_checkbox_sem_p=$_POST['tab_checkbox_sem_p']; }
 	/*************************************/
-
+	
 	// TITRE
-	if($u_login!="")
-		$login_titre = $u_login;
-	elseif($u_login_to_delete!="")
-		$login_titre = $u_login_to_delete;
+	if(isset($u_login))
+		printf("<H1>Suppression Utilisateur : %s .</H1>\n\n", $u_login);
+	elseif(isset($u_login_to_delete))
+		printf("<H1>Suppression Utilisateur : %s .</H1>\n\n", $u_login_to_delete);
 
-	echo "<H1>".$_SESSION['lang']['admin_suppr_user_titre']." : $login_titre .</H1>\n\n";
-
-	
-	if($u_login!="")
-	{
-		confirmer($u_login, $DEBUG);
+		
+	if(isset($u_login)) {
+		confirmer($u_login);
 	}
-	elseif($u_login_to_delete!="")
-	{
-		suppression($u_login_to_delete, $DEBUG);
-	}
-	else
-	{
-		// renvoit sur la page principale .
-		header("Location: admin_index.php?session=$session&onglet=admin-users");
+	else {
+		if(isset($u_login_to_delete)) {
+			suppression($u_login_to_delete);
+		}
+		else {
+			// renvoit sur la page principale .
+			header("Location: admin_index.php?session=$session");
+		}
 	}
 
-
-echo "<hr align=\"center\" size=\"2\" width=\"90%\">\n";
-
-echo "</CENTER>\n";
-
-	echo "</div>";
-	echo "</div>";
-	echo "</div>";
-	echo "</div>";
-	echo "</div>";
+function confirmer($u_login) {
+	global $PHP_SELF;
+	global $session;
 	
-	echo "<div id=\"content\">";
-	
-	echo "<div class=\"ui-corner-all-8\" style=\"background-color: #C11A22; padding: 2px; margin: 10px;\">";
-	echo "<div class=\"ui-corner-all\" style=\"background-color: white; padding: 2px;\">";
-	echo "<div class=\"ui-corner-all\" style=\"background-color: #C3C3C3; padding: 7px; margin: 3px;\">";
-	
-	echo "</div>";
-	echo "</div>";
-	echo "</div>";
-	echo "</div>";
-	
-	echo "</body>\n";
-	echo "</html>\n";
+	//connexion mysql
+	$link = connexion_mysql() ;
 
-
-
-/**************************************************************************************/
-/**********  FONCTIONS  ***************************************************************/
-
-function confirmer($u_login, $DEBUG=FALSE)
-{
-	$sql=SQL::singleton();
-	$PHP_SELF=$_SERVER['PHP_SELF'];
-	$session=session_id();
-
-	/****************************/
+	/*****************************/
 	/* Etat Utilisateur en cours */
 	/*****************************/
+	// Récupération des informations
+	$sql1 = "SELECT u_login, u_nom, u_prenom FROM conges_users WHERE u_login = '$u_login' "  ;
 	// AFFICHAGE TABLEAU
-	echo "<form action=\"$PHP_SELF?session=$session&u_login_to_delete=$u_login\" method=\"POST\">\n"  ;
-	echo "<table cellpadding=\"2\" class=\"tablo\" width=\"80%\">\n";
-	echo "<tr align=\"center\">\n";
-	echo "<td class=\"histo\">".$_SESSION['lang']['divers_login_maj_1']."</td>\n";
-	echo "<td class=\"histo\">".$_SESSION['lang']['divers_nom_maj_1']."</td>\n";
-	echo "<td class=\"histo\">".$_SESSION['lang']['divers_prenom_maj_1']."</td>\n";
-	echo "</tr>\n";
 
-	// RÃ©cupÃ©ration des informations
-	$sql1 = 'SELECT u_login, u_nom, u_prenom FROM conges_users WHERE u_login = \''.$sql->escape($u_login).'\'';
-	$ReqLog1 = requete_mysql($sql1,"confirmer", $DEBUG);
-
-	echo "<tr align=\"center\">\n";
-	while ($resultat1 = $ReqLog1->fetch_array())
-	{
-		echo "<td class=\"histo\">".$resultat1["u_login"]."</td>\n";
-		echo "<td class=\"histo\">".$resultat1["u_nom"]."</td>\n";
-		echo "<td class=\"histo\">".$resultat1["u_prenom"]."</td>\n";
+	printf("<form action=\"$PHP_SELF?session=$session&u_login_to_delete=$u_login\" method=\"POST\">\n" ) ;
+	printf("<table cellpadding=\"2\" class=\"tablo\" width=\"80%%\">\n");
+	printf("<tr align=\"center\"><td class=\"histo\">login</td><td class=\"histo\">Nom</td><td class=\"histo\">Prenom</td></tr>\n");
+	$ReqLog1 = mysql_query($sql1, $link) or die("ERREUR : mysql_query : ".$sql1." --> ".mysql_error());
+	printf("<tr>\n");
+	while ($resultat1 = mysql_fetch_array($ReqLog1)) {
+		printf("<td class=\"histo\">%s</td><td class=\"histo\">%s</td><td class=\"histo\">%s</td>\n", 
+				$resultat1["u_login"], $resultat1["u_nom"], $resultat1["u_prenom"]);
 	}
-	echo "</tr>\n";
-	echo "</table><br>\n\n";
-	echo "<input type=\"submit\" value=\"".$_SESSION['lang']['form_supprim']."\">\n";
-	echo "</form>\n" ;
+	printf("</tr>\n");
+	printf("</table><br>\n\n");
+	printf("<input type=\"submit\" value=\"Supprimer\">\n");
+	printf("</form>\n" ) ;
+	
+	printf("<form action=\"admin_index.php?session=$session\" method=\"POST\">\n" ) ;
+	printf("<input type=\"submit\" value=\"Cancel\">\n");
+	printf("</form>\n" ) ;
 
-	echo "<form action=\"admin_index.php?session=$session&onglet=admin-users\" method=\"POST\">\n"  ;
-	echo "<input type=\"submit\" value=\"".$_SESSION['lang']['form_cancel']."\">\n";
-	echo "</form>\n" ;
-
+	mysql_close($link);
 }
 
-function suppression($u_login_to_delete, $DEBUG=FALSE)
-{
-	$sql=SQL::singleton();
-	$PHP_SELF=$_SERVER['PHP_SELF'];
-	$session=session_id();
+function suppression($u_login_to_delete) {
+	global $PHP_SELF;
+	global $session;
 	//echo($u_login_to_delete."---".$u_login_to_delete."<br>");
 
-	$sql1 = 'DELETE FROM conges_users WHERE u_login = \''.$sql->escape($u_login_to_delete).'\'';
-	$result = requete_mysql($sql1, "suppression", $DEBUG);
+	//connexion mysql
+	$link = connexion_mysql() ;
+	
+	$sql1 = "DELETE FROM conges_users WHERE u_login = '$u_login_to_delete' " ;
+	$result = mysql_query($sql1, $link) or die("ERREUR : mysql_query : ".$sql1." --> ".mysql_error()) ;
 
-	$sql2 = 'DELETE FROM conges_periode WHERE p_login = \''.$sql->escape($u_login_to_delete).'\'';
-	$result2 = requete_mysql($sql2, "suppression", $DEBUG);
-
-	$sql3 = 'DELETE FROM conges_artt WHERE a_login = \''.$sql->escape($u_login_to_delete).'\'';
-	$result3 = requete_mysql($sql3, "suppression", $DEBUG);
-
-	$sql4 = 'DELETE FROM conges_echange_rtt WHERE e_login = \''.$sql->escape($u_login_to_delete).'\'';
-	$result4 = requete_mysql($sql4, "suppression", $DEBUG);
-
-	$sql5 = 'DELETE FROM conges_groupe_resp WHERE gr_login = \''.$sql->escape($u_login_to_delete).'\'';
-	$result5 = requete_mysql($sql5, "suppression", $DEBUG);
-
-	$sql6 = 'DELETE FROM conges_groupe_users WHERE gu_login = \''.$sql->escape($u_login_to_delete).'\'';
-	$result6 = requete_mysql($sql6, "suppression", $DEBUG);
-
-	$sql7 = 'DELETE FROM conges_solde_user WHERE su_login = \''.$sql->escape($u_login_to_delete).'\'';
-	$result7 = requete_mysql($sql7, "suppression", $DEBUG);
-
-
-	$comment_log = "suppression_user ($u_login_to_delete)";
-	log_action(0, "", $u_login_to_delete, $comment_log, $DEBUG);
+	$sql2 = "DELETE FROM conges_periode WHERE p_login = '$u_login_to_delete' " ;
+	$result2 = mysql_query($sql2, $link) or die("ERREUR : mysql_query : ".$sql2." --> ".mysql_error()) ;
+	
+	$sql3 = "DELETE FROM conges_artt WHERE a_login = '$u_login_to_delete' " ;
+	$result3 = mysql_query($sql3, $link) or die("ERREUR : mysql_query : ".$sql3." --> ".mysql_error()) ;
+		
+	mysql_close($link);
 
 	if($result==TRUE)
-		echo $_SESSION['lang']['form_modif_ok']." !<br><br> \n" ;
+		printf(" Changements pris en compte avec succes !<br><br> \n");
 	else
-		echo $_SESSION['lang']['form_modif_not_ok']." !<br><br> \n";
-
-	if($DEBUG==TRUE)
-	{
-		echo "<a href=\"admin_index.php?session=$session&onglet=admin-users\">".$_SESSION['lang']['form_retour']."</a>\n" ;
-	}
-	else
-	{
-		/* APPEL D'UNE AUTRE PAGE au bout d'une tempo de 2secondes */
-		echo "<META HTTP-EQUIV=REFRESH CONTENT=\"2; URL=admin_index.php?session=$session&onglet=admin-users\">";
-	}
+		printf(" ERREUR ! Changements NON pris en compte !<br><br> \n");
+	
+	/* APPEL D'UNE AUTRE PAGE au bout d'une tempo de 2secondes */
+	echo "<META HTTP-EQUIV=REFRESH CONTENT=\"2; URL=admin_index.php?session=$session\">";
 
 }
 
 ?>
+<hr align="center" size="2" width="90%">
+
+</CENTER>
+</body>
+</html>

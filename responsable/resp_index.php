d4627bd77fec3e96303d28b4e50d34f0fa9be138
holblin <holblin@gmail.com>
2012-01-17 18:44:15 +0000

    somme config

--- responsable/resp_index.php
+++ responsable/resp_index.php
@@ -65,7 +65,8 @@ verif_droits_user($session, "is_resp", $DEBUG);
 	if( $_SESSION['config']['resp_ajoute_conges'] )
 		$onglets['ajout_conges'] = _('resp_ajout_conges_titre');
 	
-	$onglets['cloture_exercice'] = _('button_cloture');
+	if (false)
+		$onglets['cloture_exercice'] = _('button_cloture');
 	
 	if ( !isset($onglets[ $onglet ]) && !in_array($onglet, array('traite_user')))
 		$onglet = 'page_principale';

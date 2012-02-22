eb28f4a8437a005bc58a84439ce5cbaa759822d9
holblin <holblin@gmail.com>
2012-02-22 11:18:39 +0000

    Ajoute le fichier et la ligne que la requete SQL

--- template/default/menu_bottom.php
+++ template/default/menu_bottom.php
@@ -22,6 +22,8 @@
 						<th>Time</th>
 						<th>Total</th>
 						<th>Results</th>
+						<th>File</th>
+						<th>Line</th>
 						<th>Query</th>
 					</tr>';
 			echo '</thead><tbody>';
@@ -35,6 +37,8 @@
 						<td>'.$time.'</td>
 						<td>'.$total.'</td>
 						<td>'.$v['results'].'</td>
+						<td>'.$v['back']['file'].'</td>
+						<td>'.$v['back']['line'].'</td>
 						<td>'.$v['query'].'</td>
 					</tr>';
 			}

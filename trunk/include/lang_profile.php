7ef2a39f385fe62ef5b3a7ea44c217cce58c693a
holblin@gmail.com <holblin@gmail.com@8d58e20b-37e7-4bb5-302f-fc13c1ed47d1>
2012-02-29 11:46:14 +0000

    fix login with lang

--- trunk/include/lang_profile.php
+++ trunk/include/lang_profile.php
@@ -22,6 +22,10 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 *************************************************************************************************/
+
+if(isset($_REQUEST['session']))
+	session_is_valid($_REQUEST['session']);
+
 if(isset($_REQUEST['lang']))
 	$lang = $_REQUEST['lang'];
 elseif(isset($_SESSION['lang']))

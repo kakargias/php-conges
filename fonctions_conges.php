976f134b8c552d6ccd72a805cbef731fe182e3b0
laurent.ghys <laurent.ghys@gmail.com>
2012-03-30 10:24:53 +0000

    missing stripslashes  when sending mails (Subject and Body) (config_mail.php uses addslashes/stripslashes when updating the DB)

--- fonctions_conges.php
+++ fonctions_conges.php
@@ -910,8 +910,8 @@ function constuct_and_send_mail($objet, $mail_sender_name, $mail_sender_addr, $m
 	$contenu = str_replace("__TYPE_ABSENCE__", $sql_type_absence, $contenu);
 
 	// construction du corps du mail
-	$mail->Subject  =  utf8_decode($sujet );
-	$mail->Body     =  utf8_decode($contenu );
+	$mail->Subject  =  stripslashes(utf8_decode($sujet ));
+	$mail->Body     =  stripslashes(utf8_decode($contenu ));
 
 
 	/*********************************************/

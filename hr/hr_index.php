1b44cbf55e317e5752e9203db71e37e99b7ce3dd
holblin <holblin@gmail.com>
2012-07-04 14:18:00 +0000

    Better is_resp_of_user function when group is used (and double validation too) Fix css 100% to 99% prevent return to new line Block a twice validation on the same vacation (and block twice count in counters)

--- hr/hr_index.php
+++ hr/hr_index.php
@@ -78,7 +78,7 @@ verif_droits_user($session, "is_hr", $DEBUG);
 	$add_css = '<style>
 		#onglet_menu { width: 100%;}
 		#onglet_menu .onglet.active{ border: 1px solid black; margin: -1px; }
-		#onglet_menu .onglet{ width: '. (100 / count($onglets) ).'% ; float: left; padding: 10px 0px 10px 0px;}
+		#onglet_menu .onglet{ width: '. (99 / count($onglets) ).'% ; float: left; padding: 10px 0px 10px 0px;}
 	</style>';
 	
 	header_menu('responsable',$_SESSION['config']['titre_resp_index'],$add_css);
